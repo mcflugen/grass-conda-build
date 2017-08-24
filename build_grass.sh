09aed60ad6859a23e70a6187d5d0f8f5d004704a
mcflugen <mcflugen@gmail.com>
2017-08-24 15:51:16 -0600

    Add script to build GRASS.

---
+++ build_grass.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+export PATH=$(pwd)/conda/bin:/usr/bin:/bin:/usr/sbin:/etc:/usr/lib
+
+curl -O https://grass.osgeo.org/grass72/source/grass-7.2.0.tar.gz
+tar xvfz grass-7.2.0.tar.gz
+
+conda create --yes -n grass python=2.7
+source activate grass
+conda install --yes --file=requirements.txt -c noaa-orr-erd -c defaults -c conda-forge
+
+patch -p0 < platform.make.in.patch
+patch -p0 < loader.py.patch
+patch -p0 < rules.make.patch
+patch -p0 < aclocal.m4.patch
+patch -p0 < install.make.patch
+patch -p0 < configure.patch
+
+cd grass-7.2.0
+bash ../configure.sh
+make -j4 GDAL_DYNAMIC=
+make install
