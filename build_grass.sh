77a00dee785f1a7e83e225896bf11490b5fb77e1
mcflugen <huttone@colorado.edu>
2017-08-25 16:00:54 -0600

    Use patches in recipe folder.

--- build_grass.sh
+++ build_grass.sh
@@ -11,12 +11,12 @@ conda create -n grass python=2.7
 source activate grass
 conda install --yes --file=requirements.txt -c noaa-orr-erd -c defaults -c conda-forge
 
-patch -p0 < platform.make.in.patch
-patch -p0 < loader.py.patch
-patch -p0 < rules.make.patch
-patch -p0 < aclocal.m4.patch
-patch -p0 < install.make.patch
-patch -p0 < configure.patch
+patch -p0 < recipe/platform.make.in.patch
+patch -p0 < recipe/loader.py.patch
+patch -p0 < recipe/rules.make.patch
+patch -p0 < recipe/aclocal.m4.patch
+patch -p0 < recipe/install.make.patch
+patch -p0 < recipe/configure.patch
 
 cd grass-7.2.2
 bash ../configure.sh
